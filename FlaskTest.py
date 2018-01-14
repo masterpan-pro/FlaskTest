b2d5eb5c1d03c7f23c2c30b02248ab2f880ef755
pengpan <327069739@qq.com>
2018-01-14 22:09:18 +0800

    2018.1.14

--- FlaskTest.py
+++ FlaskTest.py
@@ -35,22 +35,46 @@ def reversal():
 # 页面跳转和重定向
 @app.route('/redirect/<is_login>')
 def redirect_test(is_login):
-    if (is_login == '1'):
+    if is_login == '1':
         return u'没有登录'
     else:
         return redirect(url_for('login'))
 
 
 # 模板渲染和参数传递
-@app.route('/index')
-def index():
+@app.route('/index/<is_login>')
+def index(is_login):
     # return render_template('index.html', username=u'MasterPan')
     # return render_template('index.html', username=u'MasterPan', age=u'24')
-    user = {
-        'username': u'MasterPan',
-        'age': u'24'
-    }
-    return render_template('index/index.html', **user)
+    if is_login == '1':
+        user = {
+            'username': u'MasterPan',
+            'age': 24
+        }
+        # For循环
+        for k, v in user.items():
+            print(k, v)
+
+        users = [
+            {
+                'username': u'MasterPan',
+                'age': 18
+            },
+            {
+                'username': u'ZhangSan',
+                'age': 19
+            },
+            {
+                'username': u'LiSi',
+                'age': 20
+            }
+        ]
+        for item in users:
+            for k, v in item.items():
+                print(k, v)
+    else:
+        user = None
+    return render_template('index/index.html', user=user)
 
 
 if __name__ == '__main__':
